177e3a8bc6f5857f27aacdf5467b9d9b0ed8838a
bkuste2 <igorribeiro632@gmail.com>
2023-12-05 19:26:28 -0300

    🚀 feat(WIP): start sign-up page structure

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,7 +1,17 @@
 export const SignUp: React.FC = () => {
 	return (
-		<div className="">
-			<h1>SignUp</h1>
+		<div className="text-bold grid h-full w-full  lg:grid-cols-5 xl:grid-cols-3">
+			<div className="flex items-center bg-project-gray-900 lg:col-span-2 xl:col-span-1">
+				<div className="m-auto flex w-3/5 flex-col gap-4">
+					<input type="text" className="rounded-md p-3" />
+					<input type="text" className="rounded-md p-3" />
+					<input type="text" className="rounded-md p-3" />
+					<button className="h-10 w-32 self-end bg-project-blue-100">
+						Botão
+					</button>
+				</div>
+			</div>
+			<div className="hidden  bg-gradient-to-r from-project-blue-300 via-project-blue-200 to-project-blue-100 lg:col-span-3 lg:flex xl:col-span-2"></div>
 		</div>
 	)
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Home } from '@pages/home'
+import { SignUp } from './pages/sign-up'
 
 /* v6 https://reactrouter.com/en/main/upgrading/v6-data */
 
 export const router = createBrowserRouter([
 	{ path: '/', Component: Home },
+	{ path: '/sign-up', Component: SignUp },
 	/* ... all other routes */
 ])
