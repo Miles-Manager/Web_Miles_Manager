0e942818da5ad4059b175090a900a1d076497092
igorrosiak <igorluizbelterrosiak@gmail.com>
2023-12-21 01:51:02 -0300

    :sparkles: feat: new sign-up page in progress

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,36 +1,123 @@
+import { useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import useNotification, { NOTIFICATION_TYPES } from '@/hooks/useNotification'
+import { Form } from '@/components/form'
 import aircraft from '@/assets/aircraft.svg'
 
 export const SignUp: React.FC = () => {
-	return (
-		<div className="text-bold grid h-full w-full lg:grid-cols-5 xl:grid-cols-3">
-			<div className="flex items-center bg-project-blue-200 lg:col-span-2 xl:col-span-1">
-				<div className="m-auto flex w-3/5 flex-col gap-4">
-					<input type="text" placeholder="Nome" className="rounded-md p-3" />
-					<input type="email" placeholder="E-mail" className="rounded-md p-3" />
-					<input
-						type="password"
-						placeholder="Senha"
-						className="rounded-md p-3"
-					/>
-					<button className="font-semiboldbold h-10 w-[100%] self-center rounded-md bg-project-blue-100 text-white ">
-						Cadastrar
-					</button>
-					<div className="flex gap-3">
-						<span className=" text-white">Não possui uma conta?</span>
-						<a
-							className=" text-decoration-line: text-amber-500 underline"
-							href="/"
-						>
-							Criar aqui
-						</a>
-					</div>
-				</div>
-			</div>
+	const [loading, setLoading] = useState(false)
+	const notification = useNotification()
 
-			<div className="hidden h-auto w-auto bg-white lg:col-span-3 lg:flex xl:col-span-2">
-				<div className="m-auto flex h-[50vh] w-[50vw] ">
-					<img src={aircraft} alt="Aircraft" className="h-full w-full" />
-				</div>
+	const signUpSchema = z.object({
+		firstName: z.string().min(1, 'Insira seu primeiro nome'),
+		lastName: z.string().min(1, 'Insira seu último nome'),
+		cpf: z.string().min(14).max(14),
+		email: z
+			.string()
+			.email('Formato de e-mail inválido')
+			.min(1, 'Insira o email')
+			.toLowerCase(),
+		password: z.string().min(6, 'A senha precisa ter no mínimo 6 caracteres'),
+	})
+
+	type SignUpFormData = z.infer<typeof signUpSchema>
+
+	const signUpForm = useForm<SignUpFormData>({
+		resolver: zodResolver(signUpSchema),
+	})
+
+	async function signUp(data: SignUpFormData) {
+		toggleLoading(true)
+		try {
+			await SignUp(data)
+			notification(NOTIFICATION_TYPES.SUCCESS, `Login efetuado com sucesso!`)
+		} catch (error) {
+			notification(NOTIFICATION_TYPES.ERROR, `Erro ao efetuar o login.`)
+		} finally {
+			toggleLoading(false)
+		}
+	}
+
+	function toggleLoading(value: boolean) {
+		setLoading(value)
+	}
+
+	const { handleSubmit } = signUpForm
+
+	return (
+		<div className="grid h-[100vh] w-[100vw] grid-cols-2 max-md:grid-cols-1">
+			<div className="flex w-4/5 bg-project-blue-200 max-lg:w-full">
+				<FormProvider {...signUpForm}>
+					<form
+						onSubmit={handleSubmit(signUp)}
+						className="m-auto flex w-4/5 flex-col gap-5"
+					>
+						<Form.Title>Faça seu Cadastro</Form.Title>
+
+						<Form.Field>
+							<Form.Input
+								name="firstName"
+								type="text"
+								placeholder="Digite seu primeiro nome"
+								className="input-form"
+							/>
+							<Form.ErrorMessage field="firstName" />
+						</Form.Field>
+
+						<Form.Field>
+							<Form.Input
+								name="lastName"
+								type="text"
+								placeholder="Digite seu último nome"
+								className="input-form"
+							/>
+							<Form.ErrorMessage field="lastName" />
+						</Form.Field>
+
+						<Form.Field>
+							<Form.Input
+								name="cpf"
+								type="text"
+								placeholder="Digite seu CPF"
+								className="input-form"
+							/>
+							<Form.ErrorMessage field="cpf" />
+						</Form.Field>
+
+						<Form.Field>
+							<Form.Input
+								name="email"
+								type="email"
+								placeholder="Digite seu e-mail"
+								className="input-form"
+							/>
+							<Form.ErrorMessage field="email" />
+						</Form.Field>
+
+						<Form.Field>
+							<Form.Input
+								name="password"
+								type="password"
+								placeholder="Digite sua senha"
+								className="input-form"
+							/>
+							<Form.ErrorMessage field="password" />
+						</Form.Field>
+
+						<div className="flex justify-between">
+							<span>Esqueceu a senha?</span>
+							<span>Já tenho uma conta</span>
+						</div>
+						<Form.Button disabled={loading}>
+							{loading ? 'Carregando...' : 'Entrar'}
+						</Form.Button>
+					</form>
+				</FormProvider>
+			</div>
+			<div className="flex max-md:hidden">
+				<img src={aircraft} alt="Aircraft" className="m-auto h-[70vh]" />
 			</div>
 		</div>
 	)
